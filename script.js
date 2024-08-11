1a666184e7f31a30f6ba21726cf84fa59f53996f
Thomas Gurr <tpgurr21@gmail.com>
2024-08-10 20:38:01 -0400

    beginning the script

---
+++ script.js
@@ -0,0 +1,12 @@
+class Card {
+    constructor(leftP, topP, rightP, bottomP) {
+        this.leftP = leftP;
+        this.topP = topP;
+        this.rightP = rightP;
+        this.bottomP = bottomP;
+    }
+
+
+}
+const dragon = new Card(8, 9, 1, 9);
+console.log(dragon)
